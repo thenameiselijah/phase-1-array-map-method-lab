dde0f1b59967c80a9f4f2f04a8ba08bcc735eb39
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-10-23 18:40:20 -0400

    Array map

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const capitalizedWords = words.map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
 }
+
+console.log(titleCased());
